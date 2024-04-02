6fe6f128f0e8e9821ecbd9e62078d9efcdb7de99
Minjares <A01284650@tec.mx>
2024-04-02 14:44:32 -0600

    Correccion en routing

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -4,10 +4,6 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
 
-    const router = useRouter()
-    router.push('/login')
-    
-
   return (
     <div className=" ">
     
