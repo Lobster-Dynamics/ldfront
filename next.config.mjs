34cf6cae9d64ed5eaba9b713f9749a89ee2bab0b
Rodrigo Reyes <89705213+RodrigoReyes27@users.noreply.github.com>
2024-04-04 13:24:16 -0600

    Optimizar imágenes en deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: false, 
-    },
-  };
-  
-  export default nextConfig;
-  
+};
+
+export default nextConfig;
