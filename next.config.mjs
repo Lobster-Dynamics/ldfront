8f9ef6cab2ba2e5ce26d8629699da8143a73de8a
Rodrigo Reyes <89705213+RodrigoReyes27@users.noreply.github.com>
2024-04-07 14:49:51 -0600

    Configurar imágenes no optimizadas

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: "export", // (optional) Set your desired output configuration
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
