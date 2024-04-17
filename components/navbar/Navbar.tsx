6ee4e78cfbf139181539ac4607f10f72759f3dbf
Rodrigo Reyes <89705213+RodrigoReyes27@users.noreply.github.com>
2024-04-16 21:54:49 -0600

    Adaptar visualización de Navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -11,23 +11,23 @@ export default function Navbar({ isAuth }: NavbarProps) {
 		<div className="sticky inset-x-0 top-0 z-50 h-16 bg-white">
 			<header className="border-b border-gray-200 px-4">
 				<div className="flex h-16 items-center justify-between">
-					<div className="flex items-center gap-3">
+					<div className="flex items-center md:gap-3 lg:flex-grow">
 						<Link href="/">
-							<div className="flex gap-2 text-3xl">
+							<div className="flex gap-2 text-2xl lg:text-3xl">
 								<h1 className="text-purple-500">FRIDA</h1>
 								<h1 className="text-black">Research Engine</h1>
 							</div>
 						</Link>
 						{!isAuth && (
 							<span
-								className="h-8 w-px bg-gray-400"
+								className="hidden md:block h-8 w-px bg-gray-400"
 								aria-hidden="true"
 							/>
 						)}
 						{!isAuth && <SearchBar />}
 					</div>
 					{!isAuth && (
-						<div className="flex items-center gap-3">
+						<div className="hidden md:flex items-center gap-3">
 							<Link
 								href="/settings"
 								className="group transition-none focus:outline-none"
--- components/navbar/SearchBar.tsx
+++ components/navbar/SearchBar.tsx
@@ -5,12 +5,12 @@ import { Search } from "lucide-react";
 export default function SearchBar() {
 	return (
 		// TODO: Implementar SearchBar y mejorar el diseño
-		<div className="relative ml-6">
+		<div className="hidden md:block relative lg:ml-6 w-1/2">
 			<Search className="absolute bottom-0 left-2 top-0 m-auto h-8" />
 			<input
 				type="text"
 				placeholder="Buscar en mis documentos"
-				className="w-96 rounded-lg border-2 border-gray-600 py-2 pl-12 transition focus:border-purple-500 focus:outline-none"
+				className="w-80 md:96 rounded-lg border-2 lg:w-10/12 max-w-[600px] border-gray-600 py-2 pl-12 transition focus:border-purple-500 focus:outline-none"
 			/>
 		</div>
 	);
