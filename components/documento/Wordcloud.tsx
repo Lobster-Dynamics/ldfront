df75af7df4ab6f86ff0b4587dfbcabd676ccd053
Jair Santos <jsantosg0703@outlook.com>
2024-05-02 15:27:38 -0600

    Se hizo el componente de wordcloud en el visualziador

--- components/documento/Wordcloud.tsx
+++ components/documento/Wordcloud.tsx
@@ -1,25 +1,57 @@
+import React from 'react';
+import ReactWordcloud from 'react-wordcloud';
+import useSWR from 'swr';
+import { fetcher } from '@/config/fetcher';
+import { Word } from 'react-wordcloud';
 
-import Image from "next/image";
+interface WordCloudProps {
+  uuid: string;
+}
 
-export default function Wordcloud(){
+export default function Wordcloud({ uuid }: WordCloudProps) {
+  // Use SWR to fetch data from the endpoint
+  const { data, error } = useSWR<Word[]>(
+    `/document/generate_word_cloud/${uuid}`,
+    fetcher
+  );
 
+  // Handle error and loading states
+  if (error) {
+    return <div>Error fetching word cloud data.</div>;
+  }
 
-return (
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
-<div className="overflow-y-auto flex justify-center items-center h-full">
+  // Validate data structure to avoid rendering errors
+  const words = data.map((item) => {
+    if ('text' in item && 'value' in item) {
+      return { text: item.text, value: item.value };
+    } else {
+      throw new Error("Invalid data structure");
+    }
+  });
 
-        <Image
-          src="/wordcloud.png"
-          alt="WordCloud"
-          width={200}
-          height={200} 
-          className="w-6/12"
-        />
+  const options = {
+    colors: ["#AC73D9", "#8700BF", "#7B20C3", "#DBDEFF", "#AEB1F8", "#6568BB"],
+    enableTooltip: true,
+    deterministic: false,
+    fontFamily: "inter",
+    fontSizes: [35, 60],
+    fontStyle: "normal",
+    fontWeight: "normal",
+    padding: 1,
+    rotations: 3,
+    rotationAngles: [0, 75],
+    scale: "sqrt",
+    transitionDuration: 1000
+  };
 
-</div>
-
-
-
-);
-
+  // Render the ReactWordcloud with the validated data
+  return (
+    <div className="flex h-full w-full py-4 items-center justify-center overflow-y-auto">
+      <ReactWordcloud options={options} words={words} />
+    </div>
+  );
 }
