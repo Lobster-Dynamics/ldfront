f1c552c9d5281c946e4fb93cab3ce1e19ac7a1f1
Minjares <A01284650@tec.mx>
2024-05-02 21:49:41 -0600

    Imagenes

--- components/documento/Paper.tsx
+++ components/documento/Paper.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 interface Paperprops {
     title: string;
@@ -6,17 +7,38 @@ interface Paperprops {
 }
 
 export default function Paper({ title, parse }: Paperprops) {
-    
+
+    const isUrl = (text: string) => {
+        try {
+            new URL(text);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
     return (
         <div className="p-10 overflow-y-auto w-full">
             <h1 className="font-bold font-mono text-2xl mb-10">
                 {title}
             </h1>
-            {parse.map((paragraph:string,index:any) => (
-                <p key={index} className="text-xl font-mono mb-4">
-                    {paragraph}
-                </p>
-            ))}
+            {parse.map((paragraph, index) => {
+                if (isUrl(paragraph)) {
+                    // Imagen 
+                    return (
+                        <div key={index} className="mb-4">
+                            <Image src={paragraph} alt={`Image at index ${index}`} width={500} height={300} layout="responsive" />
+                        </div>
+                    );
+                } else {
+                    // Parrafo 
+                    return (
+                        <p key={index} className="text-xl font-mono mb-4">
+                            {paragraph}
+                        </p>
+                    );
+                }
+            })}
         </div>
 
     );
