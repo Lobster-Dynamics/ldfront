556ea8f1ae8a971bea2ff086291a5df2d06da416
Rodrigo Reyes <89705213+RodrigoReyes27@users.noreply.github.com>
2024-05-14 18:14:48 -0600

    Cambiar color spinner loading & alinear texto en file-explorer

--- app/(app)/file-explorer/page.tsx
+++ app/(app)/file-explorer/page.tsx
@@ -21,6 +21,7 @@ import {
 	AccordionItem,
 	AccordionTrigger,
 } from "@/components/ui/accordion"
+import PageLoader from "@/components/PageLoader/PageLoader";
 
 export default function FileExplorer() {
     const [viewMode, setViewMode] = useState<"list" | "grid">("grid");
@@ -28,8 +29,8 @@ export default function FileExplorer() {
     const searchParams = useSearchParams();
     const directoryId = searchParams?.get("id") ?? auth?.rootDirectoryId ?? "";
     const sidebardirectoryId = auth?.rootDirectoryId;
-    const { data: directoryUnparsed, isLoading } = useSWR<DirectoryDetails>(`/directory/get_directory/${directoryId}`, fetcher)
-    const { data: sidebardirectoryUnparsed } = useSWR<DirectoryDetails>(`/directory/get_directory/${sidebardirectoryId}`, fetcher)
+    const { data: directoryUnparsed, isLoading: isLoadingDirectory } = useSWR<DirectoryDetails>(`/directory/get_directory/${directoryId}`, fetcher)
+    const { data: sidebardirectoryUnparsed, isLoading: isLoadingSidebar } = useSWR<DirectoryDetails>(`/directory/get_directory/${sidebardirectoryId}`, fetcher)
     const [directory, setDirectory] = useState<DirectoryDetails | null>(null)
     const [sidebardirectory, setSidebarDirectory] = useState<DirectoryDetails | null>(null)
 
@@ -43,6 +44,8 @@ export default function FileExplorer() {
             setSidebarDirectory(loadDirectoryData(sidebardirectoryUnparsed))
         }
     }, [directoryUnparsed, sidebardirectoryUnparsed])
+
+    if (isLoadingDirectory || isLoadingSidebar) return <PageLoader />
 
     return (
         <div className="max-h-full flex-grow bg-white">
